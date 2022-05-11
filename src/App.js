f778d96a4585e6010ae9643438b423d74f9336df
dev.heeseok <dev.heeseok@gmail.com>
2022-05-12 00:17:51 +0900

    improve: React-Bootstrap Navbars Tutorial 추가 #14
    React-Bootstrap 샘플을 이용하여 샘플 추가
    - Navebars - Responsive behaviors 추가
    - Home, Questions, About Pages 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Questions from './pages/Questions'
+import About from './pages/About'
+import NavbarElements from './components/Navbars/NavbarElements';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <NavbarElements />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Questions' element={<Questions></Questions>} />
+        <Route path='/About' element={<About></About>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Questions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Questions = () => {
+  return (
+    <div>
+      <h1>Questions Pages</h1>
+    </div>
+  )
+}
+
+export default Questions
